e3287a694047307a69ca12c4658298a640139149
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2020-02-01 17:57:52 -0500

    added room class prototype and basic cleanning logic

---
+++ Assets/Scripts/House/Room_Class.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_Class : MonoBehaviour
+{
+    public enum RoomState { Broken, Fixed_Dirty, Fixed_Clean }
+    [SerializeField]
+    public RoomState roomState;
+    public enum RoomType { Kitchen, Bathroom, LivingRoom, Bedroom, Outdoor, Basement, Attic, RecRoom, WineCellar }
+    [SerializeField]
+    public RoomType roomType;
+}
---
+++ Assets/Scripts/NPC/NPC_Behaviors.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Behaviors : MonoBehaviour
+{
+    NPC_Spawner myStats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myStats = this.gameObject.GetComponent<NPC_Spawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void manageHappiness(float y)
+    {
+        myStats.happiness += y;
+    }
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        //cleanliness and cleanning rooms
+        if (c.gameObject.GetComponent<Room_Class>().roomState == Room_Class.RoomState.Fixed_Dirty)
+        {
+            if (myStats.cleanliness >= 6)
+            {
+                if (myStats.cleanliness >= 6 && myStats.like.Contains("Cleanning")) { manageHappiness(0.1f); print(":D Cleaning the " + c.gameObject.GetComponent<Room_Class>().roomType); }
+                else if (myStats.dislikes.Contains("Cleanning")) { manageHappiness(-0.05f); print(":O GROSS!! Im not cleaning that up"); }
+                else { manageHappiness(-0.2f); print(":( Cleanning the " + c.gameObject.GetComponent<Room_Class>().roomType); }
+            }
+            else print(":| This " + c.gameObject.GetComponent<Room_Class>().roomType + " is a mess");
+        }
+
+    }
+}
