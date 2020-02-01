827cf38934410483502dece0dc1860c5f8f6214c
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2020-02-01 16:32:15 -0500

    Added prototype NPC Class and spawning

---
+++ Assets/Scripts/NPC_Base.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Base : MonoBehaviour
+{
+    protected string[] names = { "Matt", "Cam", "Kyle", "Marion", "Bernadette", "Chelsea", "Leif" };
+    protected string[] likes = { "Cooking", "TV", "Fixing Things", "Quiet", "Drinking", "Gardening", "Working Out", "Cosmetics", "Cleanning"};
+    public string NPC_name;
+    public float happiness;
+    public int incomeMin;
+    public int incomeMax;
+    public string like;
+    public string dislikes;
+    public int cleanliness;
+    public  int handyness;
+
+    //public struct NPCBase
+    //{
+    //    #region things we may use
+    //    //string hates;
+    //    //string loves;
+    //    #endregion
+    //}
+    //must link the NPC attripbutes to the items in the game, e.g. if an NPC is happy cooking, their happyness is tied to the state of the kitchen
+}
---
+++ Assets/Scripts/NPC_Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Spawner : NPC_Base
+{
+    //NPC_Spawner[] characters;
+
+    private void Awake()
+    {
+        NPC_name = names[Random.Range(0, names.Length)];
+        like = likes[Random.Range(0, likes.Length)];
+        dislikes = likes[Random.Range(0, likes.Length)];
+        incomeMin = Random.Range(10, 17);
+        incomeMax = Random.Range(15, 22);
+        happiness = Random.Range(0.3f,0.7f);
+        cleanliness = Random.Range(0, 10);
+        handyness = Random.Range(0, 10);      
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (like == dislikes) dislikes = likes[Random.Range(0, likes.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
