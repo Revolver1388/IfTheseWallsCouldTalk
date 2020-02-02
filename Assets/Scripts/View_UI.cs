01402d44638eaa5d745404b91873212f3604c801
lazyninjacat <matthew.riddett@gmail.com>
2020-02-01 20:01:14 -0800

    UI stuff again

--- Assets/Scripts/View_UI.cs
+++ Assets/Scripts/View_UI.cs
@@ -14,8 +14,19 @@ public class View_UI : MonoBehaviour
     [SerializeField] Button quadSpeedButton;
     [SerializeField] Button npcManagerOpenButton;
     [SerializeField] Button npcManagerCloseButton;
+    [SerializeField] GameObject npcManager;
     [SerializeField] Button roomUpgradePanelOpenButton;
     [SerializeField] Button roomUpgradePanelCloseButton;
+    [SerializeField] GameObject KitchenRoomPanel;
+    [SerializeField] GameObject LivingRoomPanel;
+    [SerializeField] GameObject BedRoom1Panel;
+    [SerializeField] GameObject BedRoom2Panel;
+    [SerializeField] GameObject RecRoomPanel;
+    [SerializeField] GameObject WineCellarRoomPanel;
+    [SerializeField] GameObject OutDoorRoomPanel;
+    [SerializeField] GameObject BathRoomPanel;
+
+
 
 
     // Start is called before the first frame update
@@ -32,6 +43,128 @@ public class View_UI : MonoBehaviour
 
 
 
+    /// <summary>
+    /// Open/Close NPC Manager Panel
+    /// </summary>
+    public void OpenNpcManagerPanel()
+    {
+        npcManager.SetActive(true);
+    }
+
+    public void CloseNpcManagerPanel()
+    {
+        npcManager.SetActive(false);
+    }
+
+    /// <summary>
+    /// Open/Close KitchenRoomPanel
+    /// </summary>
+    public void OpenKitchenRoomPanel()
+    {
+        KitchenRoomPanel.SetActive(true);
+    }
+
+    public void CloseKitchenRoomPanel()
+    {
+        KitchenRoomPanel.SetActive(false);
+    }
+
+
+
+    /// <summary>
+    /// Open CLose LivingRoomPanel
+    /// </summary>
+    public void OpenLivingRoomPanel()
+    {
+        KitchenRoomPanel.SetActive(true);
+    }
+
+    public void CloseLivingRoomPanel()
+    {
+        LivingRoomPanel.SetActive(false);
+    }
+
+
+
+    /// <summary>
+    /// Open Close BedRoom1Panel
+    /// </summary>
+    public void OpenBedRoom1Panel()
+    {
+        BedRoom1Panel.SetActive(true);
+    }
+
+    public void CloseBedRoom1Panel()
+    {
+        BedRoom1Panel.SetActive(false);
+    }
+
+    /// <summary>
+    /// Open Close BedRoom2Panel
+    /// </summary>
+    public void OpenBedRoom2Panel()
+    {
+        BedRoom2Panel.SetActive(true);
+    }
+
+    public void CloseBedRoom2Panel()
+    {
+        BedRoom2Panel.SetActive(false);
+    }
+
+    /// <summary>
+    /// Open Close RecRoomPanel
+    /// </summary>
+    public void OpenRecRoomPanel()
+    {
+        RecRoomPanel.SetActive(true);
+    }
+
+    public void CloseRecRoomPanel()
+    {
+        RecRoomPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// Open Close WineCellarRoomPanel
+    /// </summary>
+    public void OpenWineCellarRoomPanel()
+    {
+        WineCellarRoomPanel.SetActive(true);
+    }
+
+    public void CloseWineCellarRoomPanel()
+    {
+        WineCellarRoomPanel.SetActive(false);
+    }
+
+
+    /// <summary>
+    /// Open Close OutDoorRoomPanel
+    /// </summary>
+    public void OpenOutDoorRoomPanel()
+    {
+        OutDoorRoomPanel.SetActive(true);
+    }
+
+    public void CloseOutDoorRoomPanel()
+    {
+        OutDoorRoomPanel.SetActive(false);
+    }
+
+
+    /// <summary>
+    /// Open Close OutDoorRoomPanel
+    /// </summary>
+    public void OpenBathRoomPanel()
+    {
+        BathRoomPanel.SetActive(true);
+    }
+
+    public void CloseBathRoomPanel()
+    {
+        BathRoomPanel.SetActive(false);
+    }
 
 
 
