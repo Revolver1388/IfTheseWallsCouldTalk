ced011fc4b0ae65ad28725dd5bdb906b436f0eb2
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2020-02-02 05:34:23 -0500

    Appliance class created

---
+++ Assets/Scripts/House/Appliance_Class.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Appliance_Class : MonoBehaviour
+{
+    public enum State { Broken, Fixed };
+    [System.Serializable]
+    public struct Appliance { public string name; public State state; [SerializeField] Sprite[] sprites; };
+}
