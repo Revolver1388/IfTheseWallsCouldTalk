1a9fd365f01c2530ef81ed462814c02e8fa44748
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2020-03-15 16:14:45 -0400

    UPload before switch to MobilePlatform Build

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraScript : MonoBehaviour
 {
-    public Camera main;
+    public Camera mainCam;
     Vector3 damp;
     [SerializeField] float cam_Speed;
     [SerializeField] float cam_Dist = -10;
@@ -12,13 +12,14 @@ public class Camera : MonoBehaviour
     private float horizontal;
     private float vertical;
 
-    private float zoom = 80f;
+    private float zoomMax = 80f;
 
     // Start is called before the first frame update
     void Start()
-    { 
-        if (!main) main = FindObjectOfType<Camera>();
-        main.enabled = true;
+    {
+        if (!mainCam) mainCam = Camera.main;
+        mainCam.enabled = true;
+        
     }
 
     // Update is called once per frame
@@ -27,27 +28,44 @@ public class Camera : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         if (horizontal != 0 || vertical != 0) camBehavior();
-    }
 
+        //Touch Input
+        if(Input.touchCount == 2)
+        {
+            Touch touch = Input.GetTouch(0);
+            Touch touch1 = Input.GetTouch(1);
+
+            Vector2 touchIni = touch.position - touch.deltaPosition;
+            Vector2 touch1Ini = touch1.position - touch1.deltaPosition;
+
+            float prevPosition = (touchIni - touch1Ini).magnitude;
+            float curPosition = (touch.position - touch1.position).magnitude;
+
+            float zoomMag = curPosition - prevPosition;
+
+            mainCam.orthographicSize = Mathf.Clamp(mainCam.orthographicSize - (zoomMag * 0.001f), 1, 9);
+        }
+    }
+ 
     public void camBehavior()
     {
         
-        if (main.transform.position.y <= 9 && main.transform.position.y >= 2)
+        if (mainCam.transform.position.y <= 9 && mainCam.transform.position.y >= 2)
         {
-            if (main.transform.position.x >= -10 && main.transform.position.x <= -2)
+            if (mainCam.transform.position.x >= -10 && mainCam.transform.position.x <= -2)
             {
                 if (transform.position.z < -11 || transform.position.z > -9) transform.position = new Vector3(transform.position.x, transform.position.y, cam_Dist);
                 transform.position = Vector3.SmoothDamp(transform.position, transform.position - (transform.forward - (transform.right * horizontal) - (transform.up * vertical) * distFromScreen), ref damp, cam_Speed);
             }
-            else if (main.transform.position.x < -10)
+            else if (mainCam.transform.position.x < -10)
             {
                 transform.position = new Vector3(-10, transform.position.y, cam_Dist);
             }
-            else if (main.transform.position.x > -2)
+            else if (mainCam.transform.position.x > -2)
             {
                 transform.position = new Vector3(-2f, transform.position.y, cam_Dist);
             }
-        } else if (main.transform.position.y > 9)
+        } else if (mainCam.transform.position.y > 9)
         {
             transform.position = new Vector3(transform.position.x, 9f, cam_Dist);
         }
