f4918998c65db70bda199cd565632689da98e202
lazyninjacat <matthew.riddett@gmail.com>
2020-03-17 22:45:50 -0700

    quick bug fix from renaming intro view ui script.

--- Assets/Scripts/UI/View_IntroSceneUI.cs
+++ Assets/Scripts/UI/View_IntroSceneUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class View_Intro : MonoBehaviour
+public class View_IntroSceneUI : MonoBehaviour
 {
 
     [SerializeField] GameObject aboutPanel;
